6b76d00688247b9c43fb84b8da12a969b577bf5f
royanlord <royanfarid32@gmail.com>
2024-10-12 10:49:56 +0700

    add title page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,12 +4,22 @@ import Form from "../components/Form"
 
 const Login = () => {
     return (
-        <ContainerBox>
-            <h1 className="text-4xl text-center text-[#dc213e] font-semibold">LOGIN</h1>
-            <div className="mt-10">
-                <Form />
-            </div>
-        </ContainerBox>
+        <>
+            <ContainerBox>
+                <div className="my-3">
+                    <h1 className="text-lg text-center text-[#dc213e] font-semibold">Welcome To</h1>
+                    <h2 className="text-5xl text-center text-[#dc213e] font-bold">
+                        TriviaNesia
+                    </h2>
+                </div>
+            </ContainerBox>
+            <ContainerBox>
+                <h1 className="text-4xl text-center text-[#dc213e] font-semibold">LOGIN</h1>
+                <div className="mt-10">
+                    <Form />
+                </div>
+            </ContainerBox>
+        </>
     )
 }
 
